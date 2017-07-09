6bd7e2fa5ceb6e4429e33b65931f5ecb608c2d0d
Brock Halladay <bhalladay@instructure.com>
2017-07-09 17:55:40 -0600

    updated app for page_actions and added new icons, also added a test.js that updates wordpress page

--- js/test.js
+++ js/test.js
@@ -1,2 +1,10 @@
 // document.getElementsByClassName('site-logo-image')[0].style.display = 'none';
-$('.site-logo-image').hide();
+$(document).ready(function() {
+  // $('.site-logo-image').hide();
+
+  $('#custom_panel').html('<p>updated text</p>');
+  // $('.site-description').css('color', '#A42137');
+  $('.site-description').css('color', '#2C5727');
+  $('.site-description').css('background-color', 'rgba(255,255,255,.6)');
+  $('.site-description').text('Site Enhancement Activated!');
+});
